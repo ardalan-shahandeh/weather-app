9afcbec38030508583e0694b6f2f693cf84e0e1d
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-10-22 23:51:25 +0330

    feat: Add Html Structure

--- app.vue
+++ app.vue
@@ -1,28 +1,31 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
+  <div class="h-screen relative overflow-hidden">
+    <img src="" alt="" />
+    <div class="absolute w-full h-full top-0 overlay"></div>
 
-    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-      <img
-        class="w-24 h-24 rounded-full mx-auto"
-        src="/sarah-dayan.jpg"
-        alt=""
-        width="384"
-        height="512"
-      />
-      <div class="pt-6 space-y-4">
-        <blockquote>
-          <p class="text-lg font-medium">
-            “Tailwind CSS is the only framework that I've seen scale on large
-            teams. It’s easy to customize, adapts to any design, and the build
-            size is tiny.”
-          </p>
-        </blockquote>
-        <figcaption>
-          <div>Sarah Dayan</div>
-          <div>Staff Engineer, Algolia</div>
-        </figcaption>
+    <div class="absolute w-full h-full top-0 p-48">
+      <div class="flex justify-between">
+        <div>
+          <h1 class="text-7xl text-white">Toronto</h1>
+          <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
+          <img class="w-56 icon" />
+        </div>
+
+        <div>
+          <p class="font-extralight text-9xl text-white">58 degree</p>
+        </div>
       </div>
-    </figure>
+
+      <div class="mt-20">
+        <input type="text" class="w-1/2 h-10" placeholder="Search a city"/>
+        <button class="bg-sky-400 w-20 text-white h-10">Search</button>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
